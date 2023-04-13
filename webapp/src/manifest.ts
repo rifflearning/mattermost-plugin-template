850a4499b31a8847a203f3c5a26a93a7c7cc17a3
Mike Lippert <mike@riffanalytics.ai>
2023-04-13 13:43:17 -0400

    style: add file comment

--- webapp/src/manifest.ts
+++ webapp/src/manifest.ts
@@ -1,3 +1,22 @@
+/* ******************************************************************************
+ * manifest.ts                                                                  *
+ * *************************************************************************/ /**
+ *
+ * @fileoverview The mattermost plugin.json, which contains the id and version
+ *
+ * Created on       April 13, 2023
+ * @author          Jane Developer
+ *
+ * @copyright (c) 2023-present Riff Analytics,
+ *            MIT License (see https://opensource.org/licenses/MIT)
+ *
+ * ******************************************************************************/
+
 import manifest from '@/../../plugin.json';
 
-export {manifest};
+/* **************************************************************************** *
+ * Module exports                                                               *
+ * **************************************************************************** */
+export {
+    manifest,
+};
